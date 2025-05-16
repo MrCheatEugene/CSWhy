abc70348f5e3d5cd4958f90958e03b93e2d7355c
Emil <40684650+MrCheatEugene@users.noreply.github.com>
2025-05-16 17:11:22 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,321 @@
+﻿using CS2Dumper.Offsets;
+using Swed64;
+using System.Runtime.InteropServices;
+using static CS2Dumper.Schemas.ClientDll;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Numerics;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace CSWhy
+{
+
+    class PlantedC4
+    {
+        public Vector3 position { get; set; }
+        public Vector2 position2d { get; set; }
+    }
+    class Entity
+    {
+        public string name { get; set; }
+        public string steamID { get; set; } 
+        public int health { get; set; }
+        public int ping { get; set; }
+        public int team { get; set; } // 3 - ct, 2 - t
+        public IntPtr controller { get; set; }
+        public IntPtr pawn { get; set; }
+        public Vector3 position { get; set; }
+        public Vector3 viewOffset { get; set; }
+        public Vector2 position2d { get; set; }
+        public Vector2 viewPosition2d { get; set; }
+        public Vector3 head { get; set; }
+        public Vector2 head2d { get; set; }
+        public float distance { get; set; }
+        public bool canBeAttacked { get; set; }
+    }
+
+    internal class Program
+    {
+        public static Vector2 CalculateAngles(Vector3 from, Vector3 destination)
+        {
+            float yaw;
+            float pitch;
+
+            // calculate the yaw
+            float deltaX = destination.X - from.X;
+            float deltaY = destination.Y - from.Y;
+            yaw = (float)(Math.Atan2(deltaY, deltaX) * 180 / Math.PI);
+
+            // calculate the pitch
+            float deltaZ = destination.Z - from.Z;
+            double distance = Math.Sqrt(Math.Pow(deltaX, 2) + Math.Pow(deltaY, 2));
+            pitch = -(float)(Math.Atan2(deltaZ, distance) * 180 / Math.PI);
+
+            // return calculated angles
+            return new Vector2(yaw, pitch);
+        }
+        public static Vector2 WorldToScreen(float[] matrix, Vector3 pos, Vector2 windowSize)
+        {
+            float screenW = (matrix[12] * pos.X) + (matrix[13] * pos.Y) + (matrix[14] * pos.Z) + matrix[15];
+
+            if (screenW > 0.01f)
+            {
+                float screenX = (matrix[0] * pos.X) + (matrix[1] * pos.Y) + (matrix[2] * pos.Z) + matrix[3];
+                float screenY = (matrix[4] * pos.X) + (matrix[5] * pos.Y) + (matrix[6] * pos.Z) + matrix[7];
+
+                float x = (windowSize.X / 2) + (windowSize.X / 2) * screenX / screenW;
+                float y = (windowSize.Y / 2) - (windowSize.Y / 2) * screenY / screenW;
+                return new Vector2(x, y);
+            }
+
+            return new Vector2(-99, -99);
+        }
+        static void shoot(Swed sw, bool ascope = false)
+        {
+
+            IntPtr clientBase = sw.GetModuleBase("client.dll");
+            int dwLocalPlayerPawn = (int)ClientDll.dwLocalPlayerPawn;
+            IntPtr LocalPlayerPawn = sw.ReadPointer(clientBase, dwLocalPlayerPawn);
+
+            if (ascope && !sw.ReadBool(LocalPlayerPawn, (int)C_CSPlayerPawn.m_bIsScoped) )
+            {
+                var thr = new Thread(() =>
+                {
+                    sw.WriteInt(clientBase, (int)CS2Dumper.Buttons.attack2, 65537);
+                    Thread.Sleep(10);
+                    sw.WriteInt(clientBase, (int)CS2Dumper.Buttons.attack2, 256);
+                });
+                thr.IsBackground = true;
+                thr.Start();
+            }
+            sw.WriteInt(clientBase, (int)CS2Dumper.Buttons.attack, 65537);
+            Thread.Sleep(10);
+            sw.WriteInt(clientBase, (int)CS2Dumper.Buttons.attack, 256);
+        }
+
+        static void stop(Swed sw)
+        {
+            sw.WriteInt(sw.GetModuleBase("client.dll"), (int)CS2Dumper.Buttons.forward, 65537);
+            Thread.Sleep(10);
+            sw.WriteInt(sw.GetModuleBase("client.dll"), (int)CS2Dumper.Buttons.back, 65537);
+            Thread.Sleep(10);
+            sw.WriteInt(sw.GetModuleBase("client.dll"), (int)CS2Dumper.Buttons.forward, 256);
+            sw.WriteInt(sw.GetModuleBase("client.dll"), (int)CS2Dumper.Buttons.back, 256);
+        }
+
+        static string fix_string(string str)
+        {
+            int fix = 0;
+            while (fix < str.Length && (!(str[fix] > (char)48 && str[fix] < (char)57) && !(str[fix] > (char)65 && str[fix] < (char)90) && !(str[fix] > (char)97 && str[fix] < (char)122)))
+            {
+                fix++;
+            }
+            return str.Substring(fix, str.Length - fix);
+        }
+
+
+        static public int getSelfIndex(Swed sw)
+        {
+            IntPtr clientBase = sw.GetModuleBase("client.dll");
+            int dwEntityList = (int)ClientDll.dwEntityList;
+
+            IntPtr entityList = sw.ReadPointer(clientBase, dwEntityList);
+            IntPtr listEntry = sw.ReadPointer(entityList, 0x10);
+            for (int i = 0; i < 64; i++)
+            {
+                if (listEntry == IntPtr.Zero) continue;
+                IntPtr currentController = sw.ReadPointer(listEntry, i * 0x78);
+                if (currentController == IntPtr.Zero) continue;
+                if (sw.ReadBool(currentController, (int)CBasePlayerController.m_bIsLocalPlayerController))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        static public List<Entity> getEntities(Swed sw, bool firsthead)
+        {
+            IntPtr clientBase = sw.GetModuleBase("client.dll");
+            int dwEntityList = (int)ClientDll.dwEntityList;
+            int hPlayerPawn = (int)CCSPlayerController.m_hPlayerPawn;
+            int iHealth = (int)C_BaseEntity.m_iHealth;
+            int m_iszPlayerName = (int)CBasePlayerController.m_iszPlayerName;
+
+            IntPtr entityList = sw.ReadPointer(clientBase, dwEntityList);
+            IntPtr listEntry = sw.ReadPointer(entityList, 0x10);
+            List<Entity> guys = new List<Entity>();
+            for (int i = 0; i < 64; i++)
+            {
+                if (listEntry == IntPtr.Zero) continue;
+                IntPtr currentController = sw.ReadPointer(listEntry, i * 0x78);
+                if (currentController == IntPtr.Zero) continue;
+                if(sw.ReadBool(currentController, (int)CBasePlayerController.m_bIsLocalPlayerController)) continue;
+                int pawnHandle = sw.ReadInt(currentController, hPlayerPawn);
+                if (pawnHandle == IntPtr.Zero) continue;
+                IntPtr listEntry2 = sw.ReadPointer(entityList, 0x8 * ((pawnHandle & 0x7fff) >> 9) + 0x10);
+                IntPtr currentPawn = sw.ReadPointer(listEntry2, 0x78 * (pawnHandle & 0x1ff));
+                Entity e = new Entity();
+                e.health = (int)sw.ReadUInt(currentPawn, iHealth);
+                e.name = fix_string(sw.ReadString(currentController, m_iszPlayerName, 16).Trim());
+                e.steamID = sw.ReadString(currentController, (int)CBasePlayerController.m_steamID, 32);
+                e.ping = (int)sw.ReadUInt(currentController, (int)CCSPlayerController.m_iPing);
+                e.team = (int)sw.ReadUInt(currentController, (int)C_BaseEntity.m_iTeamNum);
+                e.pawn = currentPawn;
+                e.controller = currentController;
+                e.canBeAttacked = sw.ReadBool(currentPawn + (int)C_BaseEntity.m_bTakesDamage) && (int)sw.ReadUInt(currentPawn, (int)C_CSPlayerPawnBase.m_bGunGameImmunity) !=1 ;
+                e.position = sw.ReadVec(currentPawn, (int)C_BasePlayerPawn.m_vOldOrigin);
+                e.position2d = WorldToScreen(sw.ReadMatrix(clientBase + ClientDll.dwViewMatrix), e.position, new Vector2(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height));
+                e.viewOffset = sw.ReadVec(currentPawn, (int)C_BaseModelEntity.m_vecViewOffset);
+                e.viewPosition2d = WorldToScreen(sw.ReadMatrix(clientBase + ClientDll.dwViewMatrix), Vector3.Add(e.position, e.viewOffset), new Vector2(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height));
+                int dwLocalPlayerPawn = (int)ClientDll.dwLocalPlayerPawn;
+                IntPtr LocalPlayerPawn = sw.ReadPointer(clientBase, dwLocalPlayerPawn);
+                e.distance = Vector3.Distance(e.position, sw.ReadVec(LocalPlayerPawn, (int)C_BasePlayerPawn.m_vOldOrigin));
+                IntPtr node = sw.ReadPointer(currentPawn, (int)C_BaseEntity.m_pGameSceneNode);
+                IntPtr bmatrix = sw.ReadPointer(node, (int)CSkeletonInstance.m_modelState + 0x80);
+                var rnd = new Random();
+                if (e.health > 60)
+                {
+                    e.head = sw.ReadVec(bmatrix,( firsthead ? 6 : ((int)rnd.NextInt64(2, 4))) * 32); // idk, torso?
+                }
+                else
+                {
+                    e.head = sw.ReadVec(bmatrix, (!firsthead ? 6 : ((int)rnd.NextInt64(2, 4))) * 32); // head
+                }
+                
+                e.head2d = WorldToScreen(sw.ReadMatrix(clientBase + ClientDll.dwViewMatrix), e.head, new Vector2(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height));
+                guys.Add(e);
+                if (e.canBeAttacked)
+                {
+                    //Console.WriteLine(e.name);
+                    //Console.WriteLine(e.steamID);
+                }
+            }
+            return guys;
+        }
+
+        static public void simpleMode(Swed sw) // visuals 
+        {
+            IntPtr clientBase = sw.GetModuleBase("client.dll");
+            int dwLocalPlayerPawn = (int)ClientDll.dwLocalPlayerPawn;
+            int m_pCameraServices = (int)C_BasePlayerPawn.m_pCameraServices;
+            int m_iFOV = (int)CCSPlayerBase_CameraServices.m_iFOV;
+            IntPtr LocalPlayerPawn = sw.ReadPointer(clientBase, dwLocalPlayerPawn);
+            IntPtr CameraServices = sw.ReadPointer(LocalPlayerPawn, m_pCameraServices);
+            sw.WriteUInt(CameraServices + m_iFOV, 160);
+        }
+
+        static public void noflash(Swed sw) // visuals 
+        {
+            IntPtr clientBase = sw.GetModuleBase("client.dll");
+            int dwLocalPlayerPawn = (int)ClientDll.dwLocalPlayerPawn;
+            IntPtr LocalPlayerPawn = sw.ReadPointer(clientBase, dwLocalPlayerPawn);
+            sw.WriteFloat(LocalPlayerPawn + (int)C_CSPlayerPawnBase.m_flFlashDuration, 0);
+        }
+
+        static public void setNormalFOV(Swed sw) // visuals 
+        {
+            IntPtr clientBase = sw.GetModuleBase("client.dll");
+            int dwLocalPlayerPawn = (int)ClientDll.dwLocalPlayerPawn;
+            int m_pCameraServices = (int)C_BasePlayerPawn.m_pCameraServices;
+            int m_iFOV = (int)CCSPlayerBase_CameraServices.m_iFOV;
+            IntPtr LocalPlayerPawn = sw.ReadPointer(clientBase, dwLocalPlayerPawn);
+            IntPtr CameraServices = sw.ReadPointer(LocalPlayerPawn, m_pCameraServices);
+            sw.WriteUInt(CameraServices + m_iFOV, 90);
+        }
+
+        static public void aimbot(Swed sw, List<Entity> entities, bool safe = false, bool trigger = false, bool autostop = false, bool closest = false, bool ascope = false, bool aimlock = false)
+        {
+            int sI = (int)getSelfIndex(sw);
+            IntPtr clientBase = sw.GetModuleBase("client.dll");
+            int dwLocalPlayerPawn = (int)ClientDll.dwLocalPlayerPawn;
+            IntPtr LocalPlayerPawn = sw.ReadPointer(clientBase, dwLocalPlayerPawn);
+
+            foreach (var x in entities.OrderBy(e => e.distance).ToList()) 
+            {
+                if ((!x.canBeAttacked) || x.health <= 0 || x.head2d.X == -99) { continue; }
+                if ((Vector2.Distance(x.head2d, new Vector2(Screen.PrimaryScreen.Bounds.Width / 2, Screen.PrimaryScreen.Bounds.Height / 2)) <= 140.0f) || closest) // if spinbot, we choose closest to distance
+                {
+                    //Console.WriteLine();
+                    if ((sw.ReadUInt(x.pawn + (int)C_CSPlayerPawn.m_entitySpottedState, (int)EntitySpottedState_t.m_bSpottedByMask) & (1 << sI-1) ) !=0)
+                    {
+                        var a = CalculateAngles(Vector3.Add(sw.ReadVec(LocalPlayerPawn, (int)C_BasePlayerPawn.m_vOldOrigin), sw.ReadVec(LocalPlayerPawn, (int)C_BaseModelEntity.m_vecViewOffset)), x.head);
+                        sw.WriteVec(clientBase, (int)ClientDll.dwViewAngles, new Vector3(a.Y, a.X, 0.0f));
+                        //Console.WriteLine("Write vector " + a.X + " " + a.Y);
+                        if (autostop)
+                        {
+                            stop(sw);
+                        }
+                        if (trigger)
+                        {
+                            shoot(sw, ascope);
+                        }
+                    }
+                    if (!trigger && aimlock) {
+                        var a = CalculateAngles(Vector3.Add(sw.ReadVec(LocalPlayerPawn, (int)C_BasePlayerPawn.m_vOldOrigin), sw.ReadVec(LocalPlayerPawn, (int)C_BaseModelEntity.m_vecViewOffset)), x.head);
+                        sw.WriteVec(clientBase, (int)ClientDll.dwViewAngles, new Vector3(a.Y, a.X, 0.0f));
+                    }
+                }
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Swed sw = new Swed("cs2");
+            
+            gui gui = new gui();
+            var thr = new Thread(() =>
+            {
+                gui.Start().Wait();
+            });
+            var thr2 = new Thread(() =>
+            {
+                while (true)
+                {
+                    if ((!gui.mreadonly) && gui.aimbot)
+                    {
+                        aimbot(sw, gui.ents, gui.safe, gui.trigger, gui.autostop, gui.closest, gui.ascope, gui.aimlock);
+                    }
+                }
+            });
+            var thr3 = new Thread(() =>
+            {
+                while (true)
+                {
+                    if (gui.esp | gui.trace | gui.aimbot)
+                    {
+                        gui.ents = getEntities(sw, gui.firsthead);
+                    }
+                }
+            });
+            thr.IsBackground = true;
+            thr.Start();
+            thr2.IsBackground = true;
+            thr2.Start();
+            thr3.IsBackground = true;
+            thr3.Start();
+            while (true)
+            {
+                if (!gui.mreadonly)
+                {
+                    if (gui.s1mple)
+                    {
+                        simpleMode(sw);
+                    }
+                    else
+                    {
+                        setNormalFOV(sw);
+                    }
+                    if (gui.noflash)
+                    {
+                        noflash(sw);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ gui.cs
@@ -0,0 +1,139 @@
+﻿using System.Numerics;
+using System.Runtime.InteropServices;
+using ClickableTransparentOverlay;
+using ImGuiNET;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+
+namespace CSWhy
+{
+
+    class gui: Overlay
+    {
+        [DllImport("user32.dll")]
+        static extern bool SetProcessDPIAware();
+
+        ImDrawListPtr drawList;
+        public List<Entity> ents = new List<Entity>();
+
+        public void drawESP(List<Entity> ent, PlantedC4 c4)
+        {
+            if (c4 != null)
+            {
+                Console.WriteLine(c4.position2d.X);
+                Console.WriteLine(c4.position2d.Y);
+                drawList.AddLine(new Vector2(Screen.PrimaryScreen.Bounds.Width / 2, Screen.PrimaryScreen.Bounds.Height), c4.position2d, ImGui.ColorConvertFloat4ToU32(new Vector4(1, 0, 0, 1)), 2.0f);
+                drawList.AddText(new Vector2(c4.position2d.X, c4.position2d.Y + 20.0f), ImGui.ColorConvertFloat4ToU32(new Vector4(1, 0, 0, 1)), "Planted_C4");
+            }
+            
+            foreach (Entity e in ent)
+            {
+                if (e.health >= 1)
+                {
+                    if (e.position2d.X == -99) { continue; }
+                    var color = (e.team == 2) ? ImGui.ColorConvertFloat4ToU32(new Vector4(1, 0, 0, 1)) : ImGui.ColorConvertFloat4ToU32(new Vector4(0, 0, 1, 1));
+                    float entityheight = e.position2d.Y - e.viewPosition2d.Y;
+                    Vector2 rectTop = new Vector2(e.position2d.X - entityheight / 3, e.viewPosition2d.Y);
+                    Vector2 rectBottom = new Vector2(e.position2d.X + entityheight / 3, e.position2d.Y);
+                    if (esp)
+                    {
+                        drawList.AddRect(rectTop, rectBottom, color, 0, ImDrawFlags.None, 1.5f);
+                        drawList.AddText(new Vector2(e.position2d.X - entityheight, rectBottom.Y + 10.0f), color, e.name.Trim());
+                        drawList.AddText(new Vector2(e.position2d.X - entityheight, rectBottom.Y + 20.0f), color, e.health + " HP | " + e.ping + " ms | "+ (e.canBeAttacked ? "normal" : "immune"));
+                        if(e.head2d.X == -99) { continue; }
+                        drawList.AddCircle(e.head2d, 15.0f, color, 10, 2.0f);
+                    }
+                    if (trace)
+                    {
+                        drawList.AddLine(new Vector2(Screen.PrimaryScreen.Bounds.Width / 2, Screen.PrimaryScreen.Bounds.Height), e.position2d, color, 2.0f);
+                    }
+                    if (aimbot)
+                    {
+                        drawList.AddCircle(new Vector2(Screen.PrimaryScreen.Bounds.Width / 2, Screen.PrimaryScreen.Bounds.Height/2), 70, ImGui.ColorConvertFloat4ToU32(new Vector4(1, 1, 1, 1)), 10, 2.0f);
+                    }
+                }
+            }
+        }
+
+        public bool s1mple = false;
+        public bool aimbot = false;
+        public bool autostop = false;
+        public bool esp = false;
+        public bool noflash = false;
+        public bool trigger = false;
+        public bool closest = false;
+        public bool trace = false;
+        public bool aimlock = false;
+        public bool firsthead = false;
+        public bool ascope = false;
+        private bool _show = true;
+        public bool mreadonly = false;
+        public PlantedC4 c4 = null;
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern bool SetWindowPos(
+            IntPtr hWnd,
+            IntPtr hWndInsertAfter,
+            int X, int Y, int cx, int cy,
+            uint uFlags);
+
+        static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
+        const uint SWP_SHOWWINDOW = 0x0040;
+
+
+        protected override void Render()
+        {
+
+            SetProcessDPIAware();
+            if (ImGui.IsKeyPressed(ImGuiKey.Insert))
+                _show = !_show;
+            if (_show)
+            {
+                SetWindowPos(
+                    this.window.Handle, HWND_TOPMOST,
+                    0, 0,
+                    Screen.PrimaryScreen.Bounds.Width,
+                    Screen.PrimaryScreen.Bounds.Height,
+                    SWP_SHOWWINDOW
+                );
+            }
+
+
+            var io = ImGui.GetIO();
+            ImGui.SetNextWindowPos(Vector2.Zero, ImGuiCond.Always);
+            ImGui.SetNextWindowSize(io.DisplaySize, ImGuiCond.Always);
+
+            ImGui.Begin("overlay",
+                ImGuiWindowFlags.NoDecoration
+              | ImGuiWindowFlags.NoBackground
+              | ImGuiWindowFlags.NoInputs
+              | ImGuiWindowFlags.NoMove
+              | ImGuiWindowFlags.NoScrollbar
+              | ImGuiWindowFlags.NoCollapse
+            );
+            drawList = ImGui.GetWindowDrawList();
+            drawESP(ents, c4);
+            ImGui.End();
+
+            if (!_show)
+                return;
+
+
+            ImGui.SetWindowSize(new Vector2(Screen.PrimaryScreen.WorkingArea.Width, Screen.PrimaryScreen.WorkingArea.Height));
+            ImGui.Begin("CSWhy");
+            ImGui.Checkbox("Crazy FOV", ref s1mple);
+            ImGui.Checkbox("Noflash", ref noflash);
+            ImGui.Checkbox("Aimbot", ref aimbot);
+            ImGui.Checkbox("Triggerbot (works only with aimbot)", ref trigger);
+            ImGui.Checkbox("Choose closest enemies (triggerbot)", ref closest);
+            ImGui.Checkbox("Autoscope", ref ascope);
+            ImGui.Checkbox("Autostop", ref autostop);
+            ImGui.Checkbox("Aimlock", ref aimlock);
+            ImGui.Checkbox("Prioritize headshots", ref firsthead);
+            ImGui.Checkbox("ESP / Wallhack", ref esp);
+            ImGui.Checkbox("Tracers", ref trace);
+            ImGui.Checkbox("Never write to memory", ref mreadonly);
+            ImGui.End();
+        }
+    }
+}
